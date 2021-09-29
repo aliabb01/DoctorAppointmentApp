b80e0165d7a33e5e2c6be4fc22c7a05c83035f26
DESKTOP-QTQOSUU\Rima <reema.mkh@hotmail.com>
2021-09-29 18:42:57 +0300

    sing_up & sign_in design

--- app/src/main/java/ali/app/doctorappointmentapp/MainActivity.java
+++ app/src/main/java/ali/app/doctorappointmentapp/MainActivity.java
@@ -2,13 +2,33 @@ package ali.app.doctorappointmentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button sign_in,sign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sign_in=findViewById(R.id.sign_in);
+        sign_up=findViewById(R.id.sign_up);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,sign_in.class);
+                startActivity(intent);
+            }
+        });
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,sign_up.class);
+                startActivity(intent);
+            }
+        });
     }
 }
