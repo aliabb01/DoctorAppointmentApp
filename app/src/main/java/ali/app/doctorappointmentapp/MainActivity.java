fe8bde1061e2900641921150d0db1c7c3dd93778
Ali <ali3abbasov@gmail.com>
2021-10-22 19:46:30 +0300

    refactor: Remove action bar on top with app title

--- app/src/main/java/ali/app/doctorappointmentapp/MainActivity.java
+++ app/src/main/java/ali/app/doctorappointmentapp/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /**
+         * Remove title on top
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();*/
+
         setContentView(R.layout.activity_main);
 
         btnSignIn = findViewById(R.id.btnSignIn);
