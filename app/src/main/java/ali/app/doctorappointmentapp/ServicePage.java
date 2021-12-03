dd03fd0d115218a74ee28001b9d9a39b6e6d3f42
Ali Abbasov <ali3abbasov@gmail.com>
2021-12-03 18:50:25 +0200

    add: Appointment registration notification
    In addition, update insertAppointment function
    in DBHelper to return boolean

--- app/src/main/java/ali/app/doctorappointmentapp/ServicePage.java
+++ app/src/main/java/ali/app/doctorappointmentapp/ServicePage.java
@@ -3,16 +3,22 @@ package ali.app.doctorappointmentapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Button;
@@ -32,38 +38,42 @@ import java.util.List;
 
 public class ServicePage extends AppCompatActivity {
 
-    TextView servicepage_patient,service_pageid,text_time;
+    TextView text_time_label, day, month, year;
     Button bookAppointment;
-    ImageButton pickDate;
+    ImageButton pickDate, text_time;
     ConstraintLayout appointment_layout;
     Date date;
     Time Time;
     DBHelper db;
     int h,m,s;
-    //RecyclerView recyclerView1;
+
+    boolean formValid = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_page);
-     ///   recyclerView1=findViewById(R.id.service_page);
-        servicepage_patient=findViewById(R.id.servicepage_patient);
         pickDate=findViewById(R.id.date);
-        service_pageid=findViewById(R.id.service_pageid);
         bookAppointment=findViewById(R.id.book_appointment);
         text_time=findViewById(R.id.text_time);
+        text_time_label=findViewById(R.id.text_time_label);
         appointment_layout=findViewById(R.id.appointment_layout);
+
+        day = findViewById(R.id.appointmentReg_day);
+        month = findViewById(R.id.appointmentReg_month);
+        year = findViewById(R.id.appointmentReg_year);
+
+
         /**
          * get user details
          * */
         Intent intent = getIntent();
         User user = (User) intent.getSerializableExtra("user");
-      ///  servicepage_patient.setText(String.valueOf(user.getUser_id()));
 
-       Bundle extras = getIntent().getExtras();
+        Bundle extras = getIntent().getExtras();
         String stringVariableName = extras.getString("service");
         int intVariableName = Integer.parseInt(stringVariableName);
 
-      ///  service_pageid.setText(String.valueOf(intVariableName));
         /**
          * choose the date
          * */
@@ -72,14 +82,24 @@ public class ServicePage extends AppCompatActivity {
             public void onClick(View view) {
                 AlertDialog.Builder builder=new AlertDialog.Builder(ServicePage.this);
                 builder.setView(R.layout.datepicker);
-           AlertDialog alertDialog  =builder.show();
+                AlertDialog alertDialog=builder.show();
                 CalendarView calendarView=alertDialog.findViewById(R.id.calendarView);
                 calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
                     @Override
                     public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
                         try {
+                            i1 = (i1+1) % 12;
+                            if(i1==0) {
+                                i1=12;
+                            }
                             SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
                             date=simpleDateFormat.parse(i+"-"+i1+"-"+i2);
+
+                            day.setText(String.valueOf(i2));
+                            month.setText(String.valueOf(i1));
+                            year.setText(String.valueOf(i));
+                            Log.d("SOMETAG", "onSelectedDayChange: "+i2+ " "+i1);
+
                         }catch (Exception e){
                             date=null;
                         }
@@ -104,53 +124,62 @@ public class ServicePage extends AppCompatActivity {
                       SimpleDateFormat f24Hours=new SimpleDateFormat("HH:mm");
                       try{
                           Date date1=f24Hours.parse(time);
-                          SimpleDateFormat f12Hours=new SimpleDateFormat("HH:mm aa");
-                          text_time.setText(f12Hours.format(date1));
-                      ////    Time= (java.sql.Time) f12Hours.parse(i+"-"+i1);
+                          SimpleDateFormat f24=new SimpleDateFormat("HH:mm");
+                          text_time_label.setText(f24.format(date1));
 
                       }catch (ParseException e){
-                  ///        Time=null;
                           e.printStackTrace();
                       }
                     }
-                },12,0,false);
+                },24,0,true);
                 timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 timePickerDialog.updateTime(h,m);
                 timePickerDialog.show();
-             ///   Toast.makeText(getApplicationContext(),"time"+Time,Toast.LENGTH_SHORT).show();
             }
         });
+
+        // Notification Channel
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Appointment Notification", "Appointment Notification name", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
 
         bookAppointment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                db = new DBHelper(getApplicationContext());
+                Appointment appointment = new Appointment();
+                appointment.setDate(date);
+                appointment.setTime(text_time_label.getText().toString());
 
-                    db = new DBHelper(getApplicationContext());
-                    Appointment appointment = new Appointment();
-                    appointment.setDate(date);
-                    appointment.setTime(text_time.getText().toString());
+                boolean insertAp = db.insertAppointment(appointment, intVariableName, user.getUser_id());
+                // Toast.makeText(getApplicationContext(),"register successfully",Toast.LENGTH_SHORT).show();
 
-                    db.insertAppointment(appointment, intVariableName, user.getUser_id());
-                    ////    Toast.makeText(getApplicationContext(),"register successfully",Toast.LENGTH_SHORT).show();
+                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
+
+                if(text_time_label.getText()!="") {
                     Snackbar.make(appointment_layout, "appointment created successfully", Snackbar.LENGTH_SHORT).show();
+
+                    // Notification builder here
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(ServicePage.this, "Appointment Notification");
+                    builder.setSmallIcon(R.drawable.ic_baseline_notifications_24);
+                    builder.setContentTitle("New Appointment");
+                    builder.setContentText("You have registered an appointment");
+                    builder.setStyle(new NotificationCompat.BigTextStyle()
+                            .bigText("You have registered an appointment for " + simpleDateFormat.format(appointment.getDate()) + " / " + appointment.getTime()));
+                    builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                    // Call the notification using builder instance above
+                    NotificationManagerCompat managerCompat = NotificationManagerCompat.from(ServicePage.this);
+                    managerCompat.notify(user.getUser_id(), builder.build());
+                }
+                else {
+                    Toast.makeText(ServicePage.this, "Failed to insert appointment", Toast.LENGTH_SHORT).show();
+                }
+
 
             }
         });
-
-
-
-
-       /* recyclerView1.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView1.setHasFixedSize(true);
-        DBHelper db=new DBHelper(this);
-        List<Services> servicesModel=db.getService(intVariableName);
-        if(servicesModel.size()>0) {
-            ServicePageAdapter servicesAdapter=new ServicePageAdapter(servicesModel, ServicePage.this);
-            recyclerView1.setAdapter(servicesAdapter);
-
-        }
-        else {
-            Toast.makeText(this, "no item", Toast.LENGTH_SHORT).show();
-        }*/
     }
 }
